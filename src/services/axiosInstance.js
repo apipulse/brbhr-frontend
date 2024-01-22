4833ef4273862e85b0bb8e0c354a76ddcd7e3025
Imvikram99 <120546792+Imvikram99@users.noreply.github.com>
2024-01-22 14:10:44 +0530

    added env

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8085/api/', // Update with your actual API URL
+    baseURL: import.meta.env.REACT_APP_API_BASE_URL, // Update with your actual API URL
     // You can add more default settings here
 });
 
